1f646ae8b4f7a098d0ad26aab3b5b01f857b7bd8
defrizalcahyono <defrizalcahyono@gmail.com>
2026-06-09 22:53:06 +0700

    fix code

--- src/routes/postPekerjaanRoute.js
+++ src/routes/postPekerjaanRoute.js
@@ -4,6 +4,8 @@ const postController = require("../controllers/postingPekerjaanController");
 
 router.get("/", postController.getAllPost);
 
+router.get("/applications/:idLamaranPekerjaan", postController.getDetailPelamar);
+
 router.get("/company/:idPerusahaan", postController.getAllPostByIdPerusahaan);
 
 router.get("/:idPostingan", postController.getPostinganByIdPostPekerjaan);
@@ -13,7 +15,5 @@ router.post("/", postController.createdPostinganPekerjaan);
 router.patch("/:idPostPekerjaan/status", postController.updateStatus);
 
 router.delete("/:idPostPekerjaan", postController.deletePostingan);
-
-router.get("/applications/:idLamaranPekerjaan", postController.getDetailPelamar);
 
 module.exports = router;
