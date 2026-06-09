7671a0397a56b7136d56f8534dafd8560ce3c76f
defrizalcahyono <defrizalcahyono@gmail.com>
2026-06-09 10:42:09 +0700

    fix code

--- src/index.js
+++ src/index.js
@@ -6,28 +6,27 @@ const PORT = process.env.PORT || 8080;
 
 console.log("PORT ENV =", process.env.PORT);
 
-app.listen(PORT, "0.0.0.0", () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
   console.log(chalk.green.bold("\n🚀 Server is running!\n"));
 
   console.log(
     chalk.cyan("📍 Base URL: ") +
-    chalk.whiteBright(`http://localhost:${PORT}/api/v1`)
+      chalk.whiteBright(`http://localhost:${PORT}/api/v1`)
   );
 
   console.log(
     chalk.yellow("📘 Swagger Docs: ") +
-    chalk.whiteBright(`http://localhost:${PORT}/api-docs`)
+      chalk.whiteBright(`http://localhost:${PORT}/api-docs`)
   );
 
   console.log(
     chalk.magenta("❤️ Environment: ") +
-    chalk.whiteBright(process.env.NODE_ENV || "development")
+      chalk.whiteBright(process.env.NODE_ENV || "development")
   );
 
-  if (err) {
-    console.error("LISTEN ERROR:", err);
-    return;
-  }
-
   console.log(`Listening on ${PORT}`);
+});
+
+server.on("error", (err) => {
+  console.error("LISTEN ERROR:", err);
 });
